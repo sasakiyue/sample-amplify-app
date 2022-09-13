b5f76e1568d94587ad6dfd52eed226b2bd09c950
sasakiyue <yws10lsee@gmail.com>
2022-09-14 01:35:07 +0900

    mod 1:35

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import logo from "./logo.svg";
 import "./App.css";
-import "@aws-amplify/ui-react/style.css";
-import { Amplify } from "aws-amplify";
+import "@aws-amplify/ui-react/styles.css";
+import { Amplify, Auth } from "aws-amplify";
 import { withAuthenticator } from "@aws-amplify/ui-react";
-import aws_export from "./aws-exports";
+import aws_exports from "./aws-exports";
 
-Amplify.configure(exs - exports);
+Amplify.configure(aws_exports);
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Sample React</h1>
+        <h2>
+          <a className="App-link" href="." onClick={Auth.signOut}>
+            Sign Out
+          </a>
+        </h2>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
